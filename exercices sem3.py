993a54e08a0c7f73fa2ec42d0a8f7ba8e1efca57
adel0197 <116088949+adel0197@users.noreply.github.com>
2022-11-01 11:57:19 -0400

    Update exercices sem3.py

---
+++ exercices sem3.py
@@ -0,0 +1,10 @@
+ #Créez un programme demandant à un utilisateur d'entrer un entier et retournez-lui le carré. 
+ #Si le nombre est positif, retournez-lui aussi la racine carrée.
+
+ 
+def prog(): 
+    entier = int(input("entrez entier")) 
+    carre = entier**2 
+    return carre
+print(prog())     
+   
